2ccc30baf1939219c9981441061ccae568648a7d
Jeffrey Johnson <jeffrey.johnsonj@gmail.com>
2013-07-10 02:38:07 +0000

    Read file

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var filename = "index.html";
+var file = fs.readFileSync(filename, function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+var buffer = new Buffer(file, "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
